224ef21bd4996b7fd3bff308eeda7096575cdb4f
Andrew Brainerd <drwb333@gmail.com>
2022-05-13 15:10:01 -0400

    Add properties redirect

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import { React } from 'react';
 import { Route } from 'react-router-dom';
 import Home from '../Home/Home';
+import Properties from '../Properties/Properties';
 import styles from './App.module.scss';
 
 const App = () => {
@@ -11,10 +12,8 @@ const App = () => {
           path="/apple-app-site-association"
           onEnter={() => window.location.reload()}
         />
-        <Route
-          path="/"
-          component={Home}
-        />
+        <Route exact path="/" component={Home} />
+        <Route path="/properties/:propertyId" component={Properties} />
       </div>
     </div>
   );
